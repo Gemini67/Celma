29cf838de53f53c9c6ff872f01eab605a129fe48
Rene Eng <gemini67@gmail.com>
2018-08-23 17:04:57 +0200

    Added test program for the module 'string utils'.

---
+++ src/library/common/test/test_string_util.cpp
@@ -0,0 +1,193 @@
+
+/*==
+**
+**    ####   ######  #       #    #   ####
+**   #    #  #       #       ##  ##  #    #
+**   #       ###     #       # ## #  ######    (C) 2018 Rene Eng
+**   #    #  #       #       #    #  #    #        LGPL
+**    ####   ######  ######  #    #  #    #
+**
+**
+--*/
+
+
+// module to test, header file include
+#include "celma/common/string_util.hpp"
+
+
+// Boost includes
+#define BOOST_TEST_MODULE StringUtilTest
+#include <boost/test/unit_test.hpp>
+
+
+
+/// Test the behaviour of ensure_last().
+///
+/// @since  x.y.z, 22.08.2018
+BOOST_AUTO_TEST_CASE( check_ensure_last)
+{
+
+   using celma::common::ensure_last;
+
+   // should not modify empty path
+   {
+      std::string  path;
+
+      ensure_last( path);
+
+      BOOST_REQUIRE( path.empty());
+   } // end scope
+
+   // append at most one slash
+   {
+      std::string  path( "/usr/include");
+
+      ensure_last( path);
+
+      BOOST_REQUIRE_EQUAL( path, "/usr/include/");
+
+      ensure_last( path);
+
+      BOOST_REQUIRE_EQUAL( path, "/usr/include/");
+   } // end scope
+
+   // use with other character
+   {
+      std::string  title( "Hallo");
+
+      ensure_last( title, '!');
+
+      BOOST_REQUIRE_EQUAL( title, "Hallo!");
+
+      ensure_last( title, '!');
+
+      BOOST_REQUIRE_EQUAL( title, "Hallo!");
+   } // end scope
+
+} // check_ensure_last
+
+
+
+/// Verify that remove_to_if() works correctly.
+///
+/// @since  x.y.z, 23.08.2018
+BOOST_AUTO_TEST_CASE( check_remove_to_if)
+{
+
+   using celma::common::remove_to_if;
+
+   // should not modify empty string
+   {
+      std::string  path;
+
+      remove_to_if( path, 'w');
+
+      BOOST_REQUIRE( path.empty());
+   } // end scope
+
+   // check character not found
+   {
+      std::string  str( "hello world");
+
+      remove_to_if( str, 'p');
+
+      BOOST_REQUIRE_EQUAL( str, "hello world");
+   } // end scope
+
+   // check character not found
+   {
+      std::string  str( "hello world");
+
+      remove_to_if( str, 'p', true);
+
+      BOOST_REQUIRE_EQUAL( str, "hello world");
+   } // end scope
+
+   // character found in middle of string
+   {
+      std::string  str( "hello world");
+
+      remove_to_if( str, 'o');
+
+      BOOST_REQUIRE_EQUAL( str, "orld");
+   } // end scope
+
+   // second character found in middle of string
+   {
+      std::string  str( "hello world");
+
+      remove_to_if( str, 'o', true);
+
+      BOOST_REQUIRE_EQUAL( str, "o world");
+   } // end scope
+
+   // character found in middle of string, not including
+   {
+      std::string  str( "hello world");
+
+      remove_to_if( str, 'o', false, true);
+
+      BOOST_REQUIRE_EQUAL( str, "rld");
+   } // end scope
+
+   // check character not found
+   {
+      std::string  str( "hello world");
+
+      remove_to_if( str, 'o', true, true);
+
+      BOOST_REQUIRE_EQUAL( str, " world");
+   } // end scope
+
+} // check_remove_to_if
+
+
+
+/// Verify that the shortcuts for remove_to_if() works correctly.
+///
+/// @since  x.y.z, 23.08.2018
+BOOST_AUTO_TEST_CASE( check_remove_to_if_shortcuts)
+{
+
+   // character found in middle of string
+   {
+      std::string  str( "hello world");
+
+      celma::common::remove_to_if_last_excl( str, 'o');
+
+      BOOST_REQUIRE_EQUAL( str, "orld");
+   } // end scope
+
+   // second character found in middle of string
+   {
+      std::string  str( "hello world");
+
+      celma::common::remove_to_if_first_excl( str, 'o');
+
+      BOOST_REQUIRE_EQUAL( str, "o world");
+   } // end scope
+
+   // character found in middle of string, not including
+   {
+      std::string  str( "hello world");
+
+      celma::common::remove_to_if_last_incl( str, 'o');
+
+      BOOST_REQUIRE_EQUAL( str, "rld");
+   } // end scope
+
+   // check character not found
+   {
+      std::string  str( "hello world");
+
+      celma::common::remove_to_if_first_incl( str, 'o');
+
+      BOOST_REQUIRE_EQUAL( str, " world");
+   } // end scope
+
+} // check_remove_to_if_shortcuts
+
+
+
+// =====  END OF test_string_util.cpp  =====
+
